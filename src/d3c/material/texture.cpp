fbb5007a65820286c957d4ed2ebf1184fd6fd610
Ruslan Akbashev <ruslashev@gmail.com>
2017-09-13 18:54:08 +0300

    Simplify texture class

--- src/d3c/material/texture.cpp
+++ src/d3c/material/texture.cpp
@@ -1,6 +1,7 @@
 #include "texture.hpp"
 #include "image.hpp"
 #include "../misc/log.hpp"
+#include "../../engine/utils.hh"
 
 #include <GL/glew.h>
 
@@ -8,12 +9,7 @@ Texture::Texture(const std::string& name) {
   log_init_multiple << "Initialize Texture: " << name << endl;
   m_name = name;
 
-  /* default */
-  m_tex_id = 0;
-  m_type = BYTE;
-  m_dimension = TEX_2D;
-  m_use_nearest = false;
-  m_use_clamp = false;
+  _id = 0;
 }
 
 Texture::~Texture() {
@@ -38,22 +34,21 @@ void Texture::set_num_images(int count) {
   clear();
   m_images.resize(count);
 
-  for(int i=0; i<count; i++) {
+  for (int i = 0; i < count; i++)
     m_images[i] = NULL;
-  }
 }
 
 void Texture::clear() {
-  if(m_images.size()) {
-    log_init_multiple << "Free all images of texture " << m_name << endl;
+  if (!m_images.size())
+    return;
+  log_init_multiple << "Free all images of texture " << m_name << endl;
 
-    int count = m_images.size();
+  int count = m_images.size();
 
-    for(int i=0; i<count; i++) {
-      if(m_images[i] != NULL) delete m_images[i];
-    }
-    m_images.resize(0);
-  }
+  for (int i=0; i<count; i++)
+    if (m_images[i] != NULL)
+      delete m_images[i];
+  m_images.resize(0);
 }
 
 bool Texture::upload_texture() {
@@ -66,123 +61,32 @@ bool Texture::upload_texture() {
     return false;
   }
 
-  bool created = false;
-
-  if(!m_tex_id) {
-    glGenTextures( 1, &m_tex_id );
-  }
-
-  if(m_tex_id) {
-    created = true;
-  }
+  if (!_id)
+    glGenTextures(1, &_id);
 
   GLuint format;
-  GLuint type;
-  GLuint dimension;
-
-  switch(m_images[0]->get_image_type() ) {
-    case Image::RGB:	   format = GL_RGB;
-                         break;
-    case Image::RGBA:	   format = GL_RGBA;
-                         break;
-    case Image::GREYSCALE: format = GL_ALPHA;
-                           break;
-    default: break;
+  switch (m_images[0]->get_image_type()) {
+    case Image::RGB:       format = GL_RGB;   break;
+    case Image::RGBA:      format = GL_RGBA;  break;
+    case Image::GREYSCALE: format = GL_ALPHA; break;
+    default:               format = GL_RGB;   break;
   }
 
-  switch(m_type) {
-    case BYTE:  type = GL_UNSIGNED_BYTE;
-                break;
-    case FLOAT: type = GL_FLOAT;
-                log_error << "Float type image not supported!" << endl;
-                break;
-  }
+  glBindTexture(GL_TEXTURE_2D, _id);
 
-  switch(m_dimension) {
-    case TEX_1D:   dimension = GL_TEXTURE_1D;
-                   log_debug_multiple << "type = GL_TEXTURE_1D" << endl;
-                   break;
-    case TEX_2D:   dimension = GL_TEXTURE_2D;
-                   log_debug_multiple << "type = GL_TEXTURE_2D" << endl;
-                   break;
-    default: break;
-  }
+  gluBuild2DMipmaps( GL_TEXTURE_2D, GL_COMPRESSED_RGB
+      , m_images[0]->get_image_width(), m_images[0]->get_image_height()
+      , format, GL_UNSIGNED_BYTE, m_images[0]->get_pixels());
 
-  glBindTexture( dimension, m_tex_id);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  if(m_dimension != TEX_CUBE) {
-    gluBuild2DMipmaps( dimension, /*format*/GL_COMPRESSED_RGB, m_images[0]->get_image_width(), m_images[0]->get_image_height(),
-        format, type, m_images[0]->get_pixels());
-  } else {
-    if(m_images.size()<6) {
-      log_error << "Trying to upload Cube Map with " << m_images.size() << " images" << endl;
-      return false;
-    }
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    GLuint cube_map_faces[] = {
-      GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-      GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-      GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-      GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-      GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-      GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
-    };
+  if (!_id)
+    die("failed to upload texture");
 
-    for( int i=0; i<6; i++ ) {
-      switch(m_images[i]->get_image_type() ) {
-        case Image::RGB:	   format = GL_RGB;
-                             break;
-        case Image::RGBA:	   format = GL_RGBA;
-                             break;
-        case Image::GREYSCALE: format = GL_ALPHA;
-                               break;
-        default: break;
-      }
-      gluBuild2DMipmaps( cube_map_faces[i], format, m_images[i]->get_image_width(), m_images[i]->get_image_height(),
-          format, type, m_images[i]->get_pixels() );
-    }
-  }
-
-  //		glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-
-  //Set Parameters
-  if(!m_use_nearest) {
-    glTexParameteri(dimension, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(dimension, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  } else {
-    glTexParameteri(dimension, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(dimension, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  }
-
-  if(!m_use_clamp) {
-    glTexParameteri(dimension, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(dimension, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    if(dimension == TEX_CUBE) {
-      glTexParameteri(dimension, GL_TEXTURE_WRAP_R, GL_REPEAT);
-    }
-  } else {
-    glTexParameteri(dimension, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(dimension, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    if(dimension == TEX_CUBE) {
-      glTexParameteri(dimension, GL_TEXTURE_WRAP_R, GL_CLAMP);
-    }
-  }
-
-  // if (GL_EXT_texture_filter_anisotropic_supported){
-  //   int max_anisotrophy = 1;
-  //   glGetIntegerv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max_anisotrophy);
-  //   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_anisotrophy);
-  // }
-
-  if(created) {
-    log_debug_multiple << "width " << m_images[0]->get_image_width() << ", height " << m_images[0]->get_image_height() << " bind to " << m_tex_id << endl;
-    log_debug_multiple << "mipmap = " << (m_use_nearest?"GL_NEAREST":"GL_LINEAR_MIPMAP_LINEAR") << endl;
-    log_success_multiple << "Texture binded successful" << endl;
-  } else {
-    log_debug_multiple << "glGenTextures == NULL" << endl;
-    log_error << "Texture uploaded unsuccessful" << endl;
-  }
-
-  return created;
+  return !!_id;
 }
 
--- src/d3c/material/texture.hpp
+++ src/d3c/material/texture.hpp
@@ -10,13 +10,7 @@ public:
   Texture(const std::string& name);
   ~Texture();
 
-  enum Type { BYTE, FLOAT };
-  enum Dimension { TEX_1D, TEX_2D, TEX_3D, TEX_CUBE };
-
-  void set_use_nearest( bool n_use_nearest) { m_use_nearest = n_use_nearest; }
-  void set_use_clamp( bool n_use_clamp) { m_use_clamp = n_use_clamp; }
-
-  unsigned int get_tex_id() { return m_tex_id; }
+  unsigned int get_tex_id() { return _id; }
 
   void set_image(Image* image);
   void set_image(unsigned int index, Image* image);
@@ -29,12 +23,6 @@ protected:
   std::vector<Image *> m_images;
   std::string m_name;
 
-  Type m_type;
-  Dimension m_dimension;
-
-  bool m_use_nearest;
-  bool m_use_clamp;
-
-  unsigned int m_tex_id;
+  unsigned int _id;
 };
 
